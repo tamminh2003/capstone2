c9ff5769554359ed9f82d5f23c22278e50234926
SmallKnight2314 <84006239+SmallKnight2314@users.noreply.github.com>
2022-02-09 22:28:34 +1100

    Changed MySQL connection data

--- src/db/connection.php
+++ src/db/connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$con=mysqli_connect('localhost:3307','root','','capstone_user_mockup');
+$con=mysqli_connect('sql6.freemysqlhosting.net:3306','sql6471101','aqfxY2PejE','sql6471101');
 
 if(!$con)
 {
