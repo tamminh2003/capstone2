5f9d3a5bcd17b2ce16974def4d88b7f61f335a8b
tamminh2003 <tamminh2003@gmail.com>
2022-03-02 07:41:01 +0700

    Changes

--- pages/Dashboard.php
+++ pages/Dashboard.php
@@ -3,6 +3,7 @@
 use Umpirsky\Twig\Extension\PhpFunctionExtension;
 
 require $_SERVER["DOCUMENT_ROOT"] . "/vendor/autoload.php";
+require $_SERVER["DOCUMENT_ROOT"] . "/controller/search.php";
 
 $pathToPages = $_SERVER["DOCUMENT_ROOT"] . "/pages/";
 
@@ -10,8 +11,8 @@ $twigLoader = new \Twig\Loader\FilesystemLoader($pathToPages);
 
 $twig = new Twig\Environment($twigLoader);
 
-$twig->addExtension(new PhpFunctionExtension(['str_contains']));
+$twig->addExtension(new PhpFunctionExtension(["str_contains"]));
 
-$template = $twig->load("Homepage.twig");
+$template = $twig->load("Dashboard.twig");
 
 echo $template->render(["uri" => $_SERVER["REQUEST_URI"]]);
---
+++ pages/script/script.js
@@ -0,0 +1,12 @@
+function Login_userVerif () {
+  // code...
+}
+
+function Dashboard_somescript() {
+  
+}
+
+
+// PHP view page Login.php
+
+<script src="/script/script.js"></script>
