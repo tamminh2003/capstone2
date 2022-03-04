6e7514be75c209f64713f816bc7a439ab6270e32
tamminh2003 <tamminh2003@gmail.com>
2022-03-04 07:48:27 +0700

    changes

--- pages/Dashboard.php
+++ pages/Dashboard.php
@@ -1,19 +1,19 @@
 <?php
 echo "This is Dashboard";
 
-// use Umpirsky\Twig\Extension\PhpFunctionExtension;
+use Umpirsky\Twig\Extension\PhpFunctionExtension;
 
-// require $_SERVER["DOCUMENT_ROOT"] . "/vendor/autoload.php";
-// require $_SERVER["DOCUMENT_ROOT"] . "/controller/search.php";
+require $_SERVER["DOCUMENT_ROOT"] . "/vendor/autoload.php";
+require $_SERVER["DOCUMENT_ROOT"] . "/controller/search.php";
 
-// $pathToPages = $_SERVER["DOCUMENT_ROOT"] . "/pages/";
+$pathToPages = $_SERVER["DOCUMENT_ROOT"] . "/pages/";
 
-// $twigLoader = new \Twig\Loader\FilesystemLoader($pathToPages);
+$twigLoader = new \Twig\Loader\FilesystemLoader($pathToPages);
 
-// $twig = new Twig\Environment($twigLoader);
+$twig = new Twig\Environment($twigLoader);
 
-// $twig->addExtension(new PhpFunctionExtension(["str_contains"]));
+$twig->addExtension(new PhpFunctionExtension(["str_contains"]));
 
-// $template = $twig->load("Dashboard.twig");
+$template = $twig->load("Dashboard.twig");
 
-// echo $template->render(["uri" => $_SERVER["REQUEST_URI"]]);
+echo $template->render(["uri" => $_SERVER["REQUEST_URI"]]);
