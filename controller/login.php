a05195a741777b6418f27bb7d8176f9514a18e2f
tamminh2003 <tamminh2003@gmail.com>
2022-03-04 08:01:47 +0700

    changes

--- controller/login.php
+++ controller/login.php
@@ -10,37 +10,34 @@
 //$response->setBody("hello");
 //
 //Sabre\HTTP\SAPI::sendResponse($response);
-
-require $_SERVER["DOCUMENT_ROOT"] . "/vendor/autoload.php";
-require $_SERVER["DOCUMENT_ROOT"] . "/generated-conf/config.php";
-
 use Propel\UsersQuery;
 
-$UserName = $_POST['processUsername'];
-$Password = $_POST['processPassword'];
-$retrieveUserData = UsersQuery::create()
-    ->findByUserUsername($UserName);
-$unit = $retrieveUserData->getFirst();
+try {
+	require $_SERVER["DOCUMENT_ROOT"] . "/vendor/autoload.php";
+	require $_SERVER["DOCUMENT_ROOT"] . "/generated-conf/config.php";
 
-if (!empty($unit)) {
+	$UserName = $_POST["processUsername"];
+	$Password = $_POST["processPassword"];
+	$retrieveUserData = UsersQuery::create()->findByUserUsername($UserName);
+	$unit = $retrieveUserData->getFirst();
 
-    $passwirt = $unit->getUserPassword();
-    $HashPass = password_verify($Password, $passwirt);
+	if (!empty($unit)) {
+		$passwirt = $unit->getUserPassword();
+		$HashPass = password_verify($Password, $passwirt);
 
-    if ($HashPass == false) {
-        echo "Fail";
+		if ($HashPass == false) {
+			echo "Fail";
+		} else {
+			$_SESSION["user_id"] = $unit->getUserId();
+			$_SESSION["FamilyName"] = $unit->getUserFirstname();
+			$_SESSION["GivenName"] = $unit->getUserLastname();
+			$_SESSION["user_type"] = $unit->getUserType();
 
-    } else {
-        $_SESSION['user_id'] = $unit->getUserId();
-        $_SESSION['FamilyName'] = $unit->getUserFirstname();
-        $_SESSION['GivenName'] = $unit->getUserLastname();
-        $_SESSION['user_type'] = $unit->getUserType();
-
-        echo "verified";
-
-    }
-} else {
-    echo "Fail";
-
+			echo "verified";
+		}
+	} else {
+		echo "Fail";
+	}
+} catch (Throwable $error) {
+	file_put_contents("php://stderr", $error);
 }
-
