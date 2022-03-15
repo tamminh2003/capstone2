bc259923aefc1560e8a3dc9b31d13d3068afa50f
tamminh2003 <tamminh2003@gmail.com>
2022-03-16 10:42:42 +1100

    changes for demo purposes

--- pages/SearchResult.php
+++ pages/SearchResult.php
@@ -1,15 +1,30 @@
 <?php
 
+use buzzingpixel\twigswitch\SwitchTwigExtension;
+use Umpirsky\Twig\Extension\PhpFunctionExtension;
+
 require $_SERVER["DOCUMENT_ROOT"] . "/vendor/autoload.php";
 
 require $_SERVER["DOCUMENT_ROOT"] . "/controller/search.php";
 
-$search = new Search();
+if (isset($_GET["searchMethod"])) {
+  $search = new Search();
+}
 
 $pathToPages = $_SERVER["DOCUMENT_ROOT"] . "/pages/";
+
 $twigLoader = new \Twig\Loader\FilesystemLoader($pathToPages);
+
 $twig = new Twig\Environment($twigLoader);
+
+$twig->addExtension(new PhpFunctionExtension(["str_contains"]));
+
+$twig->addExtension(new SwitchTwigExtension());
 
 $template = $twig->load("SearchResult.twig");
 
-echo $template->render(["items" => $search->getResult()]);
+if (isset($_GET["searchMethod"])) {
+  echo $template->render(["items" => $search->getResult()]);
+} else {
+  echo $template->render();
+}
