ea22d512e20880f81ce6755d5998e1f58cefcc27
tamminh2003 <tamminh2003@gmail.com>
2022-03-16 10:41:20 +1100

    added function to load dashboard dynamically

--- pages/Dashboard.php
+++ pages/Dashboard.php
@@ -1,11 +1,16 @@
 <?php
 session_start();
-var_dump($_SESSION);
 
+use buzzingpixel\twigswitch\SwitchTwigExtension;
 use Umpirsky\Twig\Extension\PhpFunctionExtension;
 
 require $_SERVER["DOCUMENT_ROOT"] . "/vendor/autoload.php";
 
+if (!$_SESSION['user_type']) {
+  $url = $_SEVER['HTTP_HOST'] . '/pages/Login.php';
+  header("Location:" . $url);
+}
+else {
 $pathToPages = $_SERVER["DOCUMENT_ROOT"] . "/pages/";
 
 $twigLoader = new \Twig\Loader\FilesystemLoader($pathToPages);
@@ -13,9 +18,12 @@ $twigLoader = new \Twig\Loader\FilesystemLoader($pathToPages);
 $twig = new Twig\Environment($twigLoader);
 
 $twig->addExtension(new PhpFunctionExtension(["str_contains"]));
+$twig->addExtension(new SwitchTwigExtension());
 
 $template = $twig->load("Dashboard.twig");
 
 $userType = $_SESSION['user_type'];
 
-echo $template->render(["uri" => $_SERVER["REQUEST_URI"], "userType" => $userType]);
+echo $template->render(["uri" => $_SERVER["REQUEST_URI"], "session" => $_SESSION]);
+}
+
