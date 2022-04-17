5226c75ad447a837d6f8332a027ace2f84c0f4ac
tamminh2003 <tamminh2003@gmail.com>
2022-04-18 09:47:04 +1000

    added device page for manufacturer

---
+++ pages/Manufacturer/Device.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+
+use buzzingpixel\twigswitch\SwitchTwigExtension;
+use Umpirsky\Twig\Extension\PhpFunctionExtension;
+
+require $_SERVER["DOCUMENT_ROOT"] . "/vendor/autoload.php";
+require $_SERVER["DOCUMENT_ROOT"] . "/controller/utility.php";
+
+const AUTHORIZED_USER = ['MANUFACTURER'];
+
+Utility\userAuthorization();
+
+$device = Utility\getDeviceById($_GET["device_id"]);
+
+$pathToPages = $_SERVER["DOCUMENT_ROOT"] . "/pages/";
+
+$twigLoader = new \Twig\Loader\FilesystemLoader($pathToPages);
+
+$twig = new Twig\Environment($twigLoader);
+
+$twig->addExtension(new PhpFunctionExtension(["str_contains"]));
+$twig->addExtension(new SwitchTwigExtension());
+
+$template = $twig->load("./Manufacturer/DeviceUpdate.twig");
+
+echo $template->render(["uri" => $_SERVER["REQUEST_URI"], "session" => $_SESSION, "device" => $device]);
