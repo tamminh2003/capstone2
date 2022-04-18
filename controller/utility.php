6879f91fc757b0ed625419ad8786d9e529ff0af2
tamminh2003 <tamminh2003@gmail.com>
2022-04-18 10:38:54 +1000

    changes

--- controller/utility.php
+++ controller/utility.php
@@ -44,30 +44,47 @@ function getDeviceById($deviceId)
   return $device;
 }
 
-function getDocumentById($docId){
-    /**fill with code for getting document by id**/
-    $docQuery = PoctDeviceAditionalInfoQuery::create()
-        ->join('PoctDeviceAditionalInfo.PoctDevice')
-        ->withColumn('PoctDevice.PoctDeviceGenericName', 'Generic')
-        ->withColumn('PoctDevice.DeviceModel', 'ModelCode')
-        ->where('PoctDeviceAditionalInfo.UserUserId = ?' , $_SESSION["user_id"])
-        ->filterByUserUserId($_SESSION["user_id"])
-        ->filterByPoctDeviceAditionalInfoType('USER_MANUAL')
-        ->findOneByPoctDeviceAditionalInfoId($docId);
-    if (!$docQuery || $docQuery == null) {
-        log("Document not found.");
-        exit();
-    }
+function getDocumentById($docId)
+{
+  /**fill with code for getting document by id**/
+  $docQuery = PoctDeviceAditionalInfoQuery::create()
+    ->join('PoctDeviceAditionalInfo.PoctDevice')
+    ->withColumn('PoctDevice.PoctDeviceGenericName', 'Generic')
+    ->withColumn('PoctDevice.DeviceModel', 'ModelCode')
+    ->where('PoctDeviceAditionalInfo.UserUserId = ?', $_SESSION["user_id"])
+    ->filterByUserUserId($_SESSION["user_id"])
+    ->filterByPoctDeviceAditionalInfoType('USER_MANUAL')
+    ->findOneByPoctDeviceAditionalInfoId($docId);
+  if (!$docQuery || $docQuery == null) {
+    log("Document not found.");
+    exit();
+  }
 
-    $document = [];
-    $document["AdditionalInfoId"] = $docQuery->getPoctDeviceAditionalInfoId();
-    $document["DeviceId"] = $docQuery->getIdpoctDevice();
-    $document["DeviceModelCode"] = $docQuery->getModelCode();
-    $document["DeviceGeneric"] = $docQuery->getGeneric();
-    $document["Label"] = $docQuery->getPoctDeviceAditionalInfoLabel();
-    $document["InfoType"] = $docQuery->getPoctDeviceAditionalInfoType();
-    $document["WebContentLink"] = $docQuery->getPoctDeviceAditionalInfoDetails();
-//    $document[""] = $docQuery->;
+  $document = [];
+  $document["AdditionalInfoId"] = $docQuery->getPoctDeviceAditionalInfoId();
+  $document["DeviceId"] = $docQuery->getIdpoctDevice();
+  $document["DeviceModelCode"] = $docQuery->getModelCode();
+  $document["DeviceGeneric"] = $docQuery->getGeneric();
+  $document["Label"] = $docQuery->getPoctDeviceAditionalInfoLabel();
+  $document["InfoType"] = $docQuery->getPoctDeviceAditionalInfoType();
+  $document["WebContentLink"] = $docQuery->getPoctDeviceAditionalInfoDetails();
+  //    $document[""] = $docQuery->;
 
-    return $document;
+  return $document;
+}
+
+function getGoogleClient()
+{
+
+  /**
+   * Set up client
+   */
+  $config = "./service_account_credentials.json";
+
+  $client = new \Google\Client();
+  $client->setApplicationName('Capstone2');
+  $client->setAuthConfig($config);
+  $client->addScope("https://www.googleapis.com/auth/drive");
+
+  return $client;
 }
