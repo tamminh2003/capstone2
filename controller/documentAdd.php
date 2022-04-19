6927b4d8bce8e292452ebdb4ebfab1efc3561704
tamminh2003 <tamminh2003@gmail.com>
2022-04-19 16:03:41 +1000

    changes

--- controller/documentAdd.php
+++ controller/documentAdd.php
@@ -4,4 +4,21 @@ require_once $_SERVER["DOCUMENT_ROOT"] ."/vendor/autoload.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/controller/utility.php";
 
 $client = Utility\getGoogleClient();
-$drive = new Google\Service\Drive();
+$drive = new Google\Service\Drive($client);
+
+$target_file = $_FILES["upload"]["tmp_name"];
+
+$file = new Google\Service\Drive\DriveFile();
+$file->setName("placholder_test.jpeg");
+
+$result = $service->files->create(
+  $file,
+  array(
+    'data' => file_get_contents($target_file),
+    'mimeType' => 'application/octet-stream',
+    'uploadType' => 'media'
+  )
+);
+
+$redirect_uri = "http://localhost:8000/pages/Manufacturer/DeviceList.php" . //device_id;
+header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
