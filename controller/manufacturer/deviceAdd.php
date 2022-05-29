ac9c3cfd75f7e8826bd7bf8fe458c5f50729429f
tamminh2003 <tamminh2003@gmail.com>
2022-05-29 14:14:34 +1000

    Create relationship from device to disease.

--- controller/manufacturer/deviceAdd.php
+++ controller/manufacturer/deviceAdd.php
@@ -18,7 +18,10 @@ defined("AUTHORIZED_USER") or define("AUTHORIZED_USER", array("MANUFACTURER"));
 require_once $_SERVER["DOCUMENT_ROOT"] . "/controller/utility.php";
 Utility\userAuthorization($_SESSION["user_type"], AUTHORIZED_USER, false);
 
+use Propel\Disease;
+use Propel\DiseaseQuery;
 use Propel\PoctDevice;
+use Propel\PoctDeviceHasDisease;
 use Propel\PoctDeviceQuery;
 use Propel\Runtime\ActiveQuery\Criteria;
 use Sabre\HTTP\Response;
@@ -33,9 +36,11 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/generated-conf/config.php";
 	"deviceImgUrl" => $deviceImgUrl,
 	"deviceType" => $deviceType,
 	"deviceDescription" => $deviceDescription,
-	"user_id" => $userId
+	"user_id" => $userId,
+	"disease" => $diseaseDetails
 ] = $_POST;
 
+// Add device to device table
 $device = new PoctDevice();
 $device->setPoctDeviceManufactureName($manufactureName);
 $device->setPoctDeviceGenericName($deviceName);
@@ -45,6 +50,34 @@ $device->setDeviceType($deviceType);
 $device->setDeviceDescripition($deviceDescription);
 $device->setUserUserId($userId);
 $saveResult = $device->save();
+
+// Check if disease exist in the table
+if (isset($diseaseDetails)) {
+	foreach ($diseaseDetails as $eachDisease) {
+		$diseaseCode = explode('-$-', $eachDisease)[0];
+		$diseaseName = explode('-$-', $eachDisease)[1];
+
+		// Search Disease table for matching disease code
+		$diseaseQuery = DiseaseQuery::create()->findByDiseaseApiKey($diseaseCode);
+
+		if ($diseaseQuery->isEmpty()) {
+			// Add disease to disease table
+			$disease = new Disease();
+			$disease->setDiseaseName($diseaseName);
+			$disease->setDiseaseApiKey($diseaseCode);
+			$disease->setDiseaseGroup("01");
+			$disease->save();
+		} else {
+			// Ignore if disease already exist.
+		}
+
+		// Associate device and disease
+		$device_disease = new PoctDeviceHasDisease();
+		$device_disease->setPoctDeviceId($device->getPoctDeviceId());
+		$device_disease->setDiseaseId($disease->getDiseaseId());
+		$device_disease->save();
+	}
+}
 
 $deviceId = PoctDeviceQuery::create()
 	->limit(1)
